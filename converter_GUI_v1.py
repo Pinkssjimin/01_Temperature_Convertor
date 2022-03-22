1a6e050888ace44901401642d6f8fcbf3f8657d2
Pinkssjimin <101675122+Pinkssjimin@users.noreply.github.com>
2022-03-22 13:16:34 +1300

    Update converter_GUI_v1.py

--- converter_GUI_v1.py
+++ converter_GUI_v1.py
@@ -10,7 +10,7 @@ class Converter:
         background_color = "light blue"
 
         # converter frame
-        self.converter_frame = Frame(width=300, bg=background_color,
+        self.converter_frame = Frame(width=600, bg=background_color,
                                      pady=10)
         self.converter_frame.grid()
 
@@ -27,12 +27,21 @@ class Converter:
                                              text="Type in the amount to be "
                                                  "converted and then push"
                                                  "one of the buttons below...",
-                                             font="Arial 1 italic", wrap=250,
+                                             font="Arial 10 italic", wrap=250,
                                              justify=LEFT, bg=background_color,
                                              padx=10, pady=10)
         self.temp_instructions_label.grid(row=1)
 
         # temperature entry box (row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20,
+                                      font="Arial 14 bold")
+        self.to_convert_entry.grid(row=2)
 
         # Conversion buttons frame (row 3)
+
+if __name__== "__main__":
+    root = Tk()
+    root.title("Temperatrue Converter")
+    something = Converter()
+    root.mainloop()
 
